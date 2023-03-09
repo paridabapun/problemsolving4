becd284154a77de6048c834ce61009fa9535fbfb
Bapun Parida <pariabapun@gmail.com>
2023-03-09 20:59:51 +0530

    Added

---
+++ src/main/java/linkedlist56/Homework1.java
@@ -0,0 +1,74 @@
+package linkedlist56;
+/*
+ * Q1. K-th element in linked list
+
+Problem Description
+You are given the head of a linked list A and an integer B. You need to find the B-th element of the linked list.
+
+Note : Follow 0-based indexing for the node numbering.
+
+
+
+Problem Constraints
+1 <= size of linked list <= 105
+
+1 <= value of nodes <= 109
+
+0 <= B < size of linked list
+
+
+
+Input Format
+The first argument A is the head of a linked list.
+
+The second arguement B is an integer.
+
+
+
+Output Format
+Return an integer.
+
+
+
+Example Input
+Input 1:
+A = 1 -> 2 -> 3
+B = 0
+Input 2:
+A = 4 -> 3 -> 2 -> 1
+B = 1
+
+
+Example Output
+Output 1:
+1
+Output 2:
+3
+
+
+Example Explanation
+For Input 1:
+The 0-th element of the linked list is 1.
+For Input 2:
+The 1-st element of the linked list is 3.
+ */
+public class Homework1 {
+	public int solve(Node A, int B) {
+		Node tail = A;
+
+		int index = 0;
+
+		while (A != null) {
+
+			if (index == B) {
+				break;
+
+			}
+			index++;
+			A = A.next;
+		}
+
+		return A.data;
+
+	}
+}
