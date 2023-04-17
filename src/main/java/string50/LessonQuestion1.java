2af9fb9291cd4d05461c7dff025b5b1ff59eaf27
Bapun Parida <pariabapun@gmail.com>
2023-04-17 17:32:38 +0530

    Added

--- src/main/java/string50/LessonQuestion1.java
+++ src/main/java/string50/LessonQuestion1.java
@@ -4,8 +4,26 @@ package string50;
 
 public class LessonQuestion1 {
 
+	public static void toggle(char ch[]) {
+
+		for (int i = 0; i < ch.length; i++) {
+
+			if (ch[i] >= 'A' && ch[i] <= 'Z') {
+				ch[i] += 32;
+			} else {
+				ch[i] -= 32;
+			}
+		}
+
+	}
+
 	public static void main(String[] args) {
-		char ch1[] = { 'b', 'a', 'c', 'd', 'a', 'b', 'e', 'c' };
+		char ch[] = { 'B', 'a', 'c', 'd', 'a', 'b', 'e', 'c' };
+		
+		toggle(ch);
+		for (int i = 0; i < ch.length; i++) {
+			System.out.print(ch[i] + " ");
+		}
 	}
 
 }
